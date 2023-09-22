9ea5815481c3aed05cdbd91a7cfbd629a41eacb0
Anna <mynameisannanas@gmail.com>
2023-09-22 12:35:19 +0200

    fix: focus subnav is open

--- source/js/modules/leaflet/leaflet.js
+++ source/js/modules/leaflet/leaflet.js
@@ -2,7 +2,7 @@ import L from '../../vendor/leaflet';
 
 let initLeaflet = () => {
   const map = L.map('map', {scrollWheelZoom: false, dragging: false}).setView([55.0286301613192, 82.9282483391627], 14);
-  L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+  L.tileLayer('https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
     maxZoom: 20,
     subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
   }).addTo(map);
